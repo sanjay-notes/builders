15b8402fbae49b35810af558007d1116f1c842e5
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2017-11-14 11:13:06 -0500

    feature:  space added between non sequential orders

--- src/library/stack/Component.jsx
+++ src/library/stack/Component.jsx
@@ -3,113 +3,148 @@ import stackManager from './StackManager';
 import './style.css';
 
 export default class Stack extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            renderLater:true,
-            stackStep: 0
-        };
-        this.renderLater = this.renderLater.bind(this);
-        this.decrementStackStep = this.decrementStackStep.bind(this);
-        this.incrementStackStep = this.incrementStackStep.bind(this);
-        this.handleShowAllSteps = this.handleShowAllSteps.bind(this);
-        stackManager.hookCallback(this.renderLater)
-    }
+	constructor(props){
+		super(props);
+		this.state = {
+			renderLater:true,
+			stackStep: 0
+		};
+		this.renderLater = this.renderLater.bind(this);
+		this.decrementStackStep = this.decrementStackStep.bind(this);
+		this.incrementStackStep = this.incrementStackStep.bind(this);
+		this.handleShowAllSteps = this.handleShowAllSteps.bind(this);
+		stackManager.hookCallback(this.renderLater)
+	}
 
-    componentWillReceiveProps(nextProps){
-        this.setState({
-            renderLater:true
-        })
-    }
+	componentWillReceiveProps(nextProps){
+		this.setState({
+			renderLater:true
+		})
+	}
 
-    renderLater(){
-        this.setState({
-            renderLater:false
-        })
-    }
+	renderLater(){
+		this.setState({
+			renderLater:false
+		})
+	}
 
-    incrementStackStep(){
-        const stepValue = this.state.stackStep < stackManager.getOrder() ?  this.state.stackStep + 1 :  this.state.stackStep;
-        this.setState({
-            stackStep:  stepValue
-        })
-    }
+	incrementStackStep(){
+		const stepValue = this.state.stackStep < stackManager.getOrder() ?  this.state.stackStep + 1 :  this.state.stackStep;
+		this.setState({
+			stackStep:  stepValue
+		})
+	}
 
-    decrementStackStep(){
-        const stepValue = this.state.stackStep > 1 ?  this.state.stackStep - 1 :  1;
+	decrementStackStep(){
+		const stepValue = this.state.stackStep > 1 ?  this.state.stackStep - 1 :  1;
 
-        this.setState({
-            stackStep:  stepValue
-        })
-    }
+		this.setState({
+			stackStep:  stepValue
+		})
+	}
 
-    handleShowAllSteps(){
-        this.setState({
-            stackStep:  0
-        })
-    }
+	handleShowAllSteps(){
+		this.setState({
+			stackStep:  0
+		})
+	}
 
-    clearAllSteps(){
-        stackManager.clearStacks(true);
-    }
+	clearAllSteps(){
+		stackManager.clearStacks(true);
+	}
 
-    componentDidMount(){
-        stackManager.trigger();
-    }
+	componentDidMount(){
+		stackManager.trigger();
+	}
 
-    render(){
-        let ui = null;
+	renderStackItem(stackObject, includeSpace, index){
+		const message = stackObject.message;
+		const type = "[" + stackObject.type + "]";
+		const order = stackObject.order + ". ";
 
-        const {renderLater, stackStep} = this.state;
+		const styleObject = {};
+		if(stackObject.type === 'event'){
+			styleObject.color = "grey";
+		}
 
-        if(!renderLater){
-
-            const stacks = stackManager.getStacks();
-            const stackIds = Object.keys(stacks);
-            ui = stackIds.map((id, index)=>{
-                const stack = stacks[id];
-                return <div key={index} className="stack-container-item">
-                    <h4>{id}</h4>
-                    <ul>
-                        {stack.map((stackObject, stackIndex)=>{
-                            if(stackStep > 0 && stackStep < stackObject.order){
-                                return null;
-                            }
-                            const styleObject = {}
-                            if(stackObject.type === 'event'){
-                                styleObject.color = "grey";
-                            }
-                            const message = stackObject.message;
-                            const type = "[" + stackObject.type + "]";
-                            const order = stackObject.order + ". ";
-                            return (<li  key={stackIndex}>
-                                <div className="stack-item" style={styleObject}>
-                                    <div className="stack-item-order">{order}</div>
-                                    <div className="stack-item-type">{type}</div>
-                                    <div className="stack-item-message">{message}</div>
-                                </div>
-                            </li>);
-                        })}
-                    </ul>
-                </div>
-            });
+		let spaceUI = null;
+		if(includeSpace){
+			spaceUI = <div style={{marginBottom:"8px"}}></div>
         }
 
-        return (<div className="stack-container">
+		return (
+            <li  key={index}>
+                {spaceUI}
+                <div className="stack-item" style={styleObject}>
+                    <div className="stack-item-order">{order}</div>
+                    <div className="stack-item-type">{type}</div>
+                    <div className="stack-item-message">{message}</div>
+                </div>
+            </li>
+		)
+    }
+
+	renderStack(stack, stackStep){
+        let prevOrderNumber;
+		return stack.map((stackObject, stackIndex) => {
+			if(stackStep > 0 && stackStep < stackObject.order){
+				return null;
+			}
+			let includeSpace = false;
+			if(prevOrderNumber === undefined){
+				prevOrderNumber = stackObject.order;
+            }
+
+            if(stackObject.order  == prevOrderNumber + 1) {
+	            includeSpace = false;
+	            prevOrderNumber = stackObject.order;
+            } else if(stackObject.order  > prevOrderNumber + 1 ){
+	            includeSpace = true;
+	            prevOrderNumber = undefined;
+            }
+			return this.renderStackItem(stackObject, includeSpace, stackIndex)
+
+		})
+    }
+
+	renderStacks(stacks, stackStep){
+		const stackIds = Object.keys(stacks);
+		return stackIds.map((id, index)=>{
+			const stack = stacks[id];
+			const ui = this.renderStack(stack, stackStep)
+			return (
+                <div key={index} className="stack-container-item">
+                    <h4>{id}</h4>
+                    <ul>{ui}</ul>
+                </div>);
+		});
+    }
+
+	render(){
+		let ui = null;
+
+		const {renderLater, stackStep} = this.state;
+
+		if(!renderLater){
+			const stacks = stackManager.getStacks();
+			ui = this.renderStacks(stacks, stackStep);
+		}
+
+		return (<div className="stack-container">
             <div className="stack-controller-container">
                 <button onClick={this.decrementStackStep}>Prev step</button>
                 <div>
-                 <button onClick={this.handleShowAllSteps}>Show All steps</button>
-                 <button onClick={this.clearAllSteps}>Clear</button>
+                    <button onClick={this.handleShowAllSteps}>Show All steps</button>
+                    <button onClick={this.clearAllSteps}>Clear</button>
                 </div>
                 <button onClick={this.incrementStackStep}>Next step</button>
             </div>
 
             <div className="stack-message-container">
-                {ui}
+				{ui}
             </div>
         </div>)
-    }
+	}
 }
 
 
